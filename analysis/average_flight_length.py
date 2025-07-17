c4c2c553fe7072faae19c2ef23e8231ac238f237
Kilian Bartsch <kilianbartsch@yahoo.com>
2025-07-17 09:53:42 +0100

    moving all analysis py files into analysis folder

--- analysis/average_flight_length.py
+++ analysis/average_flight_length.py
@@ -4,4 +4,4 @@ global_rpk_per_year = 9.0e12
 passengers_per_year = 5.0e9
 
 average_flight_length = aviation.average_flight_length(global_rpk_per_year, passengers_per_year)
-print(f"Average flight length: {average_flight_length=:.0f} km")
+print(f"Average flight length: {average_flight_length=:,.0f} km")
--- analysis/required_global_fleet.py
+++ analysis/required_global_fleet.py
@@ -8,7 +8,7 @@ days_per_year = 366.0
 passengers_per_day = aviation.passengers_per_day(passengers_per_year, days_per_year)
 required_global_fleet = aviation.required_global_fleet(passengers_per_day, seats_per_aircraft, flights_per_day)
 
-print(f"Required global fleet: {required_global_fleet=:.0f} aircraft")
+print(f"Required global fleet: {required_global_fleet=:,.0f} aircraft")
 
 
 
