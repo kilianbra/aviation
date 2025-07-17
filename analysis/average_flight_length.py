3976c23db3bf95969860bc57b0a9b8f95daaf87f
Kilian Bartsch <kilianbartsch@yahoo.com>
2025-07-17 10:44:41 +0100

    after ruff formated

--- analysis/average_flight_length.py
+++ analysis/average_flight_length.py
@@ -3,5 +3,7 @@ import aviation
 global_rpk_per_year = 9.0e12
 passengers_per_year = 5.0e9
 
-average_flight_length = aviation.average_flight_length(global_rpk_per_year, passengers_per_year)
+average_flight_length = aviation.average_flight_length(
+    global_rpk_per_year, passengers_per_year
+)
 print(f"Average flight length: {average_flight_length=:,.0f} km")
