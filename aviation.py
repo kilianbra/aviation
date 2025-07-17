bbc0870538d7e748889f16a8e1224bc9741e8ddf
Kilian Bartsch <kilianbartsch@yahoo.com>
2025-07-17 08:55:57 +0100

    refactored the functions and moved only models in aviation.py and then created new model functions for each functionality in three new analysis files

--- aviation.py
+++ aviation.py
@@ -1,20 +1,12 @@
-days_per_year = 366.0
-passengers_per_year = 5.0e9
-global_rpk_per_year = 9.0e12
 
-aircraft_flights_per_day = 3.0
-seats_per_aircraft = 250.0
+def passengers_per_day(passengers_per_year, days_per_year):
+    return passengers_per_year / days_per_year
 
-passengers_per_day = passengers_per_year / days_per_year
-required_global_fleet = passengers_per_day / (seats_per_aircraft * aircraft_flights_per_day)
+def required_global_fleet(passengers_per_day, seats_per_aircraft, aircraft_flights_per_day):
+    return passengers_per_day / (seats_per_aircraft * aircraft_flights_per_day)
 
-print(f"Required global fleet: {required_global_fleet=:.0f} aircraft")
-
-average_flight_length = global_rpk_per_year / passengers_per_year
-print(f"Average flight length: {average_flight_length=:.0f} km")
-
-
-
+def average_flight_length(global_rpk_per_year, passengers_per_year):
+    return global_rpk_per_year / passengers_per_year
 
 
 
