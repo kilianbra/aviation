3cca0d1dd0665bb3e17d4d0353e54eb5c22a3f5b
Kilian Bartsch <kilianbartsch@yahoo.com>
2025-07-17 10:12:44 +0100

    changed understanding of importing in __init vs using __all

--- src/aviation/__init__.py
+++ src/aviation/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["required_global_fleet", "passengers_per_day", "average_flight_length"]
+__all__ = ["required_global_fleet", "passengers_per_day", "average_flight_length"] # Makes them imported when import *
 
 
 from aviation.fleet import required_global_fleet, passengers_per_day, average_flight_length
@@ -6,10 +6,10 @@ from aviation.fleet import required_global_fleet, passengers_per_day, average_fl
 # this represents the package name and the module
 
 # Now we have added the functions in the fleet module to the package namespace
-# This means we can now import the functions directly from the package namespace (thanks to the __all__)
+# This means we can now import the functions directly from the package namespace (thanks to the import in __init__.py)
 # so we can import aviation and then use aviation.fleet.passengers_per_day(passengers_per_year, days_per_year)
 # or can directly call aviation.passengers_per_day(passengers_per_year, days_per_year)
 # We can also from aviation import fleet and then use fleet.passengers_per_day(passengers_per_year, days_per_year)
 # or from aviation import passengers_per_day and then use passengers_per_day(passengers_per_year, days_per_year)
 # or finally from aviation.fleet import passengers_per_day and then use passengers_per_day(passengers_per_year, days_per_year)
-# (this would have been required if we hadn't added the functions to the package namespace with the __all__)
+# (this would have been required if we hadn't added the functions to the package namespace thanks to the import in __init__.py
