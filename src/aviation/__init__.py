e11d40977b4fe74e3d9ba79f8c4d8c2aec50643f
Kilian Bartsch <kilianbartsch@yahoo.com>
2025-07-17 09:53:09 +0100

    refactoring aviation.py into fleet.py in aviation package

---
+++ src/aviation/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["required_global_fleet", "passengers_per_day", "average_flight_length"]
+
+
+from aviation.fleet import required_global_fleet, passengers_per_day, average_flight_length
+# the first aviation is the folder name, the second is the file name
+# this represents the package name and the module
+
