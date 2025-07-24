a4a9c334192a82056c91e4ae2b5680f461921cda
Kilian Bartsch <kilianbartsch@yahoo.com>
2025-07-24 15:37:18 +0100

    vectorising and using array inputs to transforms

--- analysis/passengers_per_year.py
+++ analysis/passengers_per_year.py
@@ -6,9 +6,9 @@ from camia_model.units import percent, year
 import aviation
 
 # Parameters
-start_year = 2025
+years = list(range(2025, 2051))
 num_years = 26
-demand_growth_rate = 1.0 * percent / year  # Example growth rate
+demand_growth_rate = 4.0 * percent / year  # Example growth rate
 
 total_passengers = 0.0
 final_year_value = None
@@ -16,19 +16,16 @@ final_year_value = None
 systems_model = engine.SystemsModel(aviation.transforms)
 output = "passengers_per_year"
 
-for i in range(num_years):
-    current_year = start_year + i
-    inputs = {
-        "modelling_year": current_year,
-        "demand_growth_rate": demand_growth_rate,
-    }
-    passengers_per_year = systems_model.evaluate(inputs, output)
-    total_passengers += passengers_per_year.value
-    if i == num_years - 1:
-        final_year_passengers = passengers_per_year
+inputs = {
+    "modelling_year": years,
+    "demand_growth_rate": demand_growth_rate,
+}
+passengers_per_year = systems_model.evaluate(inputs, output)
 
-print(f"Total passengers over {num_years} years: {total_passengers:.3e}")
-print(
-    f"Passengers in final year ({start_year + num_years - 1}): {final_year_passengers.value:,.0f} "
-    f"{final_year_passengers.unit}"
-)
+list_result = [
+    f"In year {year}: {k.value:>15,.0f} {k.unit} \n"
+    for year, k in zip(
+        passengers_per_year.coords["modelling_year"].values, passengers_per_year.values, strict=True
+    )
+]
+print("".join(list_result))
